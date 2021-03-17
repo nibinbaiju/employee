e6d08c140e8836edfee4b072bf3c0b9b913782f8
nibinbaiju <nibinbaiju@gmail.com>
2021-03-17 14:33:45 +0530

    reyna

---
+++ resources/views/EMPL.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul class="nav nav-pills">
+<li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="/">ADD</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="/search">SEARCH</a>
+  </li>
+  
+
+</ul>
+ <div class="container">
+ <div class="row">
+ <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+</div>
+ <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+ <table class="table">
+ <tr>
+     <td>Name</td>
+     <td><input type="text" class="form-control"></td>
+ </tr>
+ <tr>
+
+ <tr>
+     <td>SALARY</td>
+     <td><input type="text" class="form-control"></td>
+ </tr>
+ 
+ <tr>
+     <td>DESIGNATION</td>
+     <td><input type="text" class="form-control"></td>
+ </tr>
+ <tr>
+     <td>DOB</td>
+     <td><date class="form-control"></date></td>
+ </tr>
+ <tr>
+     <td>PLACE</td>
+     <td><input type="text" class="form-control"></td>
+ </tr>
+ <tr>
+     <td>PINCODE</td>
+     <td><input type="text" class="form-control"></td>
+ </tr>
+ <tr>
+     <td>PHONENO</td>
+     <td><input type="text" class="form-control"></td>
+ </tr>
+ 
+ 
+ 
+ 
+ <tr>
+     <td></td>
+     <td><Button class="btn btn-secondary">Submit</Button></td>
+ </tr>
+
+
+ </table>
+ </div>
+ <div class="<div col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4"></div>
+
+ </div>
+ </div>   
+</body>
+</html>
